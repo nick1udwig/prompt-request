2eaa5658f9796c74c90cbe7ba16cc02faa9719c7
Nick Ludwig <hosted-fornet@proton.me>
2026-02-01 05:13:47 +0000

    dont redirect from dev server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "/h/"
-});
+export default defineConfig(({ command }) => ({
+  // Use /h/ for production builds, but avoid dev-server redirecting / -> /h/.
+  base: command === "build" ? "/h/" : "/"
+}));
